82b1db8d26104cbfee4907228ea00e44783a3659
binjoo <icesword28@qq.com>
2020-05-15 10:25:10 +0800

    guid

---
+++ guid/preload.js
@@ -0,0 +1,15 @@
+const crypto = require('crypto');
+window.UUIDGenerator = () => ([1e7] + 1e3 + 4e3 + 8e3 + 1e11).replace(/[018]/g, c => (c ^ (crypto.randomBytes(1)[0] & (15 >> (c / 4)))).toString(16))
+
+window.exports = {
+    "guid": {
+        mode: "none",
+        args: {
+            enter: (action) => {
+                utools.hideMainWindow()
+                utools.copyText(window.UUIDGenerator())
+                utools.outPlugin()
+            }
+        }
+    }
+}
